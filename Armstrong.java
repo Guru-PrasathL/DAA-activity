9d26b0f122b08c502f798d9f96bd7879d4bbd51c
Guru-PrasathL <gurulogu2018@gmail.com>
2025-05-18 17:21:34 +0530

    Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,11 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int num = 153, sum = 0, temp = num;
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+        System.out.println(num == sum ? "Armstrong" : "Not Armstrong");
+    }
+}
